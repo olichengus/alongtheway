d2a80e76f002ea31914a22d2e5e04ce9c7879d81
olicheng <olicheng@student.ubc.ca>
2024-10-27 22:35:57 -0700

    Convert txt to csv

--- server/server.js
+++ server/server.js
@@ -5,4 +5,6 @@ app.get('/api', (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] });
 });
 
-app.listen(5001, () => {console.log('Server is running on port 5001')});
+app.listen(5001, () => {
+    console.log('Server is running on port 5001')
+});
