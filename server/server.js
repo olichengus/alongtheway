12d5fab1e3d3719356eaed407391756a7449475c
olicheng <olicheng@student.ubc.ca>
2024-10-29 16:29:26 -0700

    Slow query of bus stops

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';  
 
-app.get('/api', (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"] });
+dotenv.config();  
+const app = express();  
+const supabaseUrl = process.env.SUPABASE_URL;
+const key = process.env.SUPABASE_KEY;  
+const supabase = createClient(supabaseUrl, key);
+
+app.get('/stops', async (req, res) => {
+    let route_short_name = req.query.route_short_name;
+    if (!route_short_name) {
+        return res.status(400).json({ error: 'route_short_name is required' });
+    }
+    if (route_short_name.length === 2 && Number.isInteger(route_short_name) && parsenInt(route_short_name) < 100) {
+            route_short_name = '0' + route_short_name;
+    }
+    const { data, error } = await supabase
+        .from('trips_routes')
+        .select('stop_name')
+        .eq('route_short_name', route_short_name)
+        .eq('direction_id', 0);
+    if (error) {
+        return res.status(500).json({ error: error.message });
+    }
+    if (data.length === 0) {
+        return res.status(404).json({ error: 'route_short_name not found' });
+    }
+    // const stops = await supabase.rpc('get_stops', { route_id: id });
+    res.json({ "stops": data});
 });
 
 app.listen(5001, () => {
--- server/supabase/startupScript.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const url = process.env.SUPABASE_URL
-const key = process.env.SUPABASE_KEY
-const supabase = createClient(url, key)
-
-export const startupScript = () => {
-};
